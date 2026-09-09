31c6aa74bfaa9db943adb81fbf975b15f9335288
jaimini279 <jaminitrivedi47@gmail.com>
2026-09-09 21:38:09 +0530

    Add day29

---
+++ day29/question57.c
@@ -0,0 +1,27 @@
+// Q57: Find the sum of array elements.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, sum = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
---
+++ day29/question58.c
@@ -0,0 +1,39 @@
+// Q58: Find the maximum and minimum element in an array.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    max = a[0];
+    min = a[0];
+
+    for (i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+
+    printf("Max=%d, Min=%d", max, min);
+
+    return 0;
+}
+

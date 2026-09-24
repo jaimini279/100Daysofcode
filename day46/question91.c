ab38381f45465f7058caef5ead6bcfb704913a26
Jamini Trivedi <JaiminiTrivedi.40849@stu.upes.ac.in>
2026-09-24 05:48:09 +0530

    Add day46

---
+++ day46/question91.c
@@ -0,0 +1,30 @@
+// Remove all vowels from a string.
+
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, j = 0;
+
+    printf("Enter a string: ");
+    fgets(str, 100, stdin);
+
+    for (i = 0; str[i] != '\0' && str[i] != '\n'; i++)
+    {
+        if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' &&
+            str[i] != 'o' && str[i] != 'u' && str[i] != 'A' &&
+            str[i] != 'E' && str[i] != 'I' && str[i] != 'O' &&
+            str[i] != 'U')
+        {
+            str[j] = str[i];
+            j++;
+        }
+    }
+
+    str[j] = '\0';
+
+    printf("%s", str);
+
+    return 0;
+}
